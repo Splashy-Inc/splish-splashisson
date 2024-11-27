ba9ab77f029c5c55488a90933d9f504b43bd6615
avelitj12 <avelitj12@gmail.com>
2024-11-27 14:31:40 -0500

    Add animations based on walking direction

--- Scenes/splish.gd
+++ Scenes/splish.gd
@@ -9,8 +9,13 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction:
+		if direction.y < 0:
+			$AnimatedSprite2D.play("walking_up")
+		else:
+			$AnimatedSprite2D.play("walking_down")
 		velocity = direction * SPEED
 	else:
+		$AnimatedSprite2D.play("idle")
 		velocity = Vector2.ZERO
 
 	move_and_slide()
