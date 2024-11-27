5d5a199ed133b20346c1a0aac4a35577bce311ac
avelitj12 <avelitj12@gmail.com>
2024-11-27 14:20:49 -0500

    Use WASD for movement

--- Scenes/splish.gd
+++ Scenes/splish.gd
@@ -7,10 +7,10 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
 
 	move_and_slide()
