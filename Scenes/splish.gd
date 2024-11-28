1b9f6fb434ff9e8aad588be2d1f9cf4df1af8aef
avelitj12 <avelitj12@gmail.com>
2024-11-28 13:53:41 -0500

    Add assigning crew to marker2d

--- Scenes/splish.gd
+++ Scenes/splish.gd
@@ -32,9 +32,18 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("interact"):
 		if interactables.front().has_method("set_assignment"):
 			interactables.front().set_assignment(self)
+	if Input.is_action_just_pressed("location"):
+		if interactables.front().has_method("set_assignment"):
+			interactables.front().set_assignment(place_location_marker())
 
 func _on_interactable_range_body_entered(body: Node2D) -> void:
 	interactables.append(body)
 
 func _on_interactable_range_body_exited(body: Node2D) -> void:
 	interactables.erase(body)
+
+func place_location_marker():
+	var new_marker = Marker2D.new()
+	new_marker.global_position = global_position
+	get_parent().add_child(new_marker)
+	return new_marker
