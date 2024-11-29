ea40f5c624fe0291d51eca8b4d1225a62e94f550
avelitj12 <avelitj12@gmail.com>
2024-11-29 17:33:45 -0500

    Refactor task to support worker vs assignee
    Assignee means crew that is assigned to do the thing
    
    Worker is crew actively working on the task
    
    Helps Player logic know if task is already assigned, avoiding issue where 2 crew can try to do the same task

--- Scripts/task.gd
+++ Scripts/task.gd
@@ -2,38 +2,25 @@ extends StaticBody2D
 
 class_name Task
 
-@export var test_assignee: Crew
 var assignee: Crew
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	_test_assignment()
+var worker: Crew
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 # This function is intended to be set when the respective crew is in range of the task to begin work
-func set_assignee(new_assignee: Crew):
-	if new_assignee: # Only allow setting assignee if not already taken
-		if assignee == null:
-			new_assignee.hide_self()
-			new_assignee.global_position = global_position
+func set_worker(new_worker: Crew) -> bool:
+	if new_worker: # Only allow setting assignee if not already taken
+		if worker == null:
+			new_worker.hide_self()
+			new_worker.global_position = global_position
 			$AnimatedSprite2D.play("active")
-	elif assignee: # Null being passed in means unassigning current assignee, so show them
-		assignee.show_self()
+		else:
+			return false
+	elif worker: # Null being passed in means unassigning current assignee, so show them
+		worker.show_self()
 		$AnimatedSprite2D.play("idle")
 	
-	assignee = new_assignee
-
-func _test_assignment():
-	if test_assignee:
-		print(self, " waiting 5 seconds to assign ", test_assignee)
-		await get_tree().create_timer(5).timeout
-		set_assignee(test_assignee)
-		print(test_assignee, " assigned to ", self)
-		print(self, " waiting 5 seconds to unassign ", test_assignee)
-		await get_tree().create_timer(5).timeout
-		set_assignee(null)
-		print(test_assignee, " unassigned from ", self)
+	worker = new_worker
+	return true
