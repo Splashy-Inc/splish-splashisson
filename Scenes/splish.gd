9a9d2b8c92657efc154e7e9d9118c0ec80c4534c
avelitj12 <avelitj12@gmail.com>
2024-11-29 11:24:08 -0500

    Point left/right based on where interact target is

--- Scenes/splish.gd
+++ Scenes/splish.gd
@@ -8,22 +8,23 @@ const JUMP_VELOCITY = -400.0
 var interactables: Array[Node2D]
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("left", "right", "up", "down")
-	if direction:
-		if direction.y < 0:
-			$AnimatedSprite2D.play("walking_up")
+	if $AnimatedSprite2D.animation != "pointing_right" or not $AnimatedSprite2D.is_playing():
+		var direction := Input.get_vector("left", "right", "up", "down")
+		if direction:
+			if direction.y < 0:
+				$AnimatedSprite2D.play("walking_up")
+			else:
+				$AnimatedSprite2D.play("walking_down")
+			velocity = direction * SPEED
 		else:
-			$AnimatedSprite2D.play("walking_down")
-		velocity = direction * SPEED
-	else:
-		$AnimatedSprite2D.play("idle")
-		velocity = Vector2.ZERO
-	
-	move_and_slide()
-	for i in get_slide_collision_count():
-		var col = get_slide_collision(i)
-		if col.get_collider() is Crew:
-			col.get_collider().push(velocity.normalized().rotated(deg_to_rad(90)) * SPEED * 2)
+			$AnimatedSprite2D.play("idle")
+			velocity = Vector2.ZERO
+		
+		move_and_slide()
+		for i in get_slide_collision_count():
+			var col = get_slide_collision(i)
+			if col.get_collider() is Crew:
+				col.get_collider().push(velocity.normalized().rotated(deg_to_rad(90)) * SPEED * 2)
 			
 func _unhandled_key_input(event: InputEvent) -> void:
 	# Identify closest interactable as action target
@@ -37,10 +38,14 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if action_target and action_target.has_method("set_assignment"):
 		if Input.is_action_just_pressed("unassign"):
 			action_target.set_assignment(null)
+			point(action_target.global_position)
 		if Input.is_action_just_pressed("interact"):
 			action_target.set_assignment(self)
+			point(action_target.global_position)
 		if Input.is_action_just_pressed("location"):
 			action_target.set_assignment(place_location_marker())
+			point(action_target.global_position)
+		
 
 func _on_interactable_range_body_entered(body: Node2D) -> void:
 	interactables.append(body)
@@ -53,3 +58,10 @@ func place_location_marker():
 	new_marker.global_position = global_position
 	get_parent().add_child(new_marker)
 	return new_marker
+
+func point(target_position: Vector2):
+	if global_position.direction_to(target_position).x < 0:
+		$AnimatedSprite2D.flip_h = true
+	$AnimatedSprite2D.play("pointing_right")
+	await $AnimatedSprite2D.animation_finished
+	$AnimatedSprite2D.flip_h = false
