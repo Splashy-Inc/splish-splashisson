a25e20de94d10305c120b19d8b6e783c8d7f4a8a
avelitj12 <avelitj12@gmail.com>
2024-12-01 15:51:28 -0500

    Add SFX manager to rowing task
    Also update it for spatial audio support

--- Scripts/sfx_manager.gd
+++ Scripts/sfx_manager.gd
@@ -2,7 +2,8 @@ extends Node
 
 func play(sound_name: String):
 		if sound_name:
-			if get_node(sound_name) is AudioStreamPlayer:
-				get_node(sound_name).play()
+			var sound_node = get_node(sound_name)
+			if sound_node is AudioStreamPlayer or sound_node is AudioStreamPlayer2D:
+				sound_node.play()
 			else:
-				print("Requested sound ", sound_name, " is either not a child of ", self, ", or not an AudioStreamPlayer")
+				print("Requested sound ", sound_name, " is either not a child of ", self, ", or not an AudioStreamPlayer/2D")
