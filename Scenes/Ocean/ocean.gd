14567cb313ee6ee8fa44af82a274a82ea4333a83
Tristan <avelitj12@gmail.com>
2024-12-24 10:25:50 -0500

    Implement workaround for ocean parallax resetting

--- Scenes/Ocean/ocean.gd
+++ Scenes/Ocean/ocean.gd
@@ -11,4 +11,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_speed_updated():
-	autoscroll.y = clamp(Globals.boat_speed/5, 0, 400)
+	# Offset the ocean based on the position before updating the autoscroll (WORKAROUND)
+	# There is a known "issue" with the parallax resetting when autoscroll is updated,
+	#   which has a solution in 4.4, which does not have a stable release as of writing:
+	#   https://github.com/godotengine/godot/issues/97605
+	var cur_position = position
+	set_autoscroll(Vector2(0, clamp(Globals.boat_speed/5, 0, 400)))
+	scroll_offset.y = cur_position.y
