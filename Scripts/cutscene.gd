cbd4007267979ec11f7fc0d76b848c4f520f0d81
avelitj12 <avelitj12@gmail.com>
2025-02-08 15:33:08 -0500

    Actually make press any button to contiure work

--- Scripts/cutscene.gd
+++ Scripts/cutscene.gd
@@ -14,5 +14,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton or event is InputEventJoypadButton or event is InputEventKey:
+		_on_continue_pressed()
+
 func _on_continue_pressed() -> void:
 	start_pressed.emit()
