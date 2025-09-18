8222f288c3f590e3db0aedd0c82904fe31a2afbe
avelitj12 <avelitj12@gmail.com>
2025-09-18 07:48:51 -0400

    Add tutorial option to levels menu
    Placeholder pulses when no levels unlocked yet. Playing or skipping the tutorial unlocks level 1.
    
    Also reorganize flow of how levels load to go to the levels menu between each completed level and reveal connecting line for the next stage

--- SCRIPTS/map_path.gd
+++ SCRIPTS/map_path.gd
@@ -3,16 +3,17 @@ extends Line2D
 class_name MapPath
 
 @export var connected_button : Button
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var is_selected := true
+var is_revealed := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if is_instance_valid(connected_button):
+		modulate = Color.TRANSPARENT
 		connected_button.mouse_entered.connect(set_highlight.bind(true))
 		connected_button.mouse_exited.connect(set_highlight.bind(false))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	visible = not connected_button.disabled
 
@@ -21,3 +22,11 @@ func set_highlight(is_enable: bool):
 		if not connected_button.stage_data.unlocked:
 			return
 	material.set_shader_parameter("on", is_enable)
+
+func reveal_self():
+	is_revealed = true
+	animation_player.play("reveal")
+
+func _on_visibility_changed() -> void:
+	if visible and not is_revealed:
+		reveal_self()
