afbdea0820924565b7da63340d413376a087e50e
avelitj12 <avelitj12@gmail.com>
2025-10-10 08:25:39 -0400

    Livestock cargo new threat behavior
    Don't restore cargo damage when all threats removed
    
    Cargo always degrading

--- SCRIPTS/cargo.gd
+++ SCRIPTS/cargo.gd
@@ -94,12 +94,19 @@ func get_self_polygon():
 
 func _on_damage_tick_timer_timeout() -> void:
 	if not level_complete:
-		if threats.is_empty() and item_health:
-			var remainder = condition % item_health
-			if remainder > 0:
-				_update_condition(clamp(item_health - (condition % item_health), 0, item_health))
-		else:
-			_update_condition(-threats.size())
+		match cargo_type:
+			Cargo_type.LIVESTOCK:
+				_update_condition(-(1 + threats.size()))
+			_:
+				if threats.is_empty() and item_health:
+					restore_condition()
+				else:
+					_update_condition(-threats.size())
+
+func restore_condition():
+	var remainder = condition % item_health
+	if remainder > 0:
+		_update_condition(clamp(item_health - (condition % item_health), 0, item_health))
 
 func _update_condition(change: int):
 	condition = clamp(condition + change, 0, max_condition)
