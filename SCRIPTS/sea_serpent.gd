52d5fdadd575fa228ecc71f78ac539782ba38a5b
Tristan <avelitj12@gmail.com>
2025-12-11 17:01:47 -0500

    Sea serpent swim around boat

--- SCRIPTS/sea_serpent.gd
+++ SCRIPTS/sea_serpent.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+@export var path_follow_node : PathFollow2D
+@export var target : Node2D
 @export var flip := false
+
+@export var speed := 200
+
+@onready var animation_tree: AnimationTree = $AnimationPlayer/AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	animation_tree.get("parameters/playback").travel("Swim")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if target:
+		flip = global_position.x < target.global_position.x
+	
+	if path_follow_node:
+		path_follow_node.progress += speed * delta
