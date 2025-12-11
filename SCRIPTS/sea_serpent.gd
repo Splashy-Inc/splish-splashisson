674c9f62603e683481d633155722e7cc20c8fee1
Tristan <avelitj12@gmail.com>
2025-12-11 17:38:24 -0500

    Serpent action rotation

--- SCRIPTS/sea_serpent.gd
+++ SCRIPTS/sea_serpent.gd
@@ -7,15 +7,59 @@ extends Node2D
 @export var speed := 200
 
 @onready var animation_tree: AnimationTree = $AnimationPlayer/AnimationTree
+var state_machine : AnimationNodeStateMachinePlayback
+
+enum Actions {
+	SWIM,
+	CHOMP,
+	JUMP,
+}
+
+var current_action := Actions.SWIM
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_tree.get("parameters/playback").travel("Swim")
+	state_machine = animation_tree.get("parameters/playback")
+	state_machine.travel("Swim")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if target:
 		flip = global_position.x < target.global_position.x
 	
-	if path_follow_node:
-		path_follow_node.progress += speed * delta
+	match state_machine.get_current_node():
+		"Hidden":
+			if path_follow_node:
+				path_follow_node.progress += speed * delta * 2
+			
+			match current_action:
+				Actions.SWIM:
+					if target is RowingTask and abs(global_position.y - target.global_position.y) < 4:
+						global_position.y = target.global_position.y + 24
+						state_machine.travel("Chomp")
+						current_action = Actions.CHOMP
+				Actions.CHOMP:
+					if target is RowingTask and abs(global_position.y - target.global_position.y) < 4:
+						global_position.y = target.global_position.y + 24
+						state_machine.travel("Jump")
+						current_action = Actions.JUMP
+				Actions.JUMP:
+					path_follow_node.progress_ratio = .12
+					state_machine.travel("Swim")
+					current_action = Actions.SWIM
+		"Chomp":
+			pass
+		"Idle":
+			state_machine.travel("Hidden")
+		"Jump":
+			pass
+		"Rise":
+			pass
+		"Sink":
+			pass
+		"Swim":
+			if path_follow_node:
+				if path_follow_node.progress_ratio > 0 and path_follow_node.progress_ratio < .05:
+					state_machine.travel("Hidden")
+					target = get_tree().get_nodes_in_group("rowing_task").pick_random()
+				path_follow_node.progress += speed * delta
