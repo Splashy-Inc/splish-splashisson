Splashy-Inc/splish-splashisson

commit 2f51f71d20f56b3cdadb7422a09329ef22371c19

Crew members wait for next same type task
Instead of going idle, crew search for any newly spawned tasks of the same type every .1 seconds

SCRIPTS/crew_member.gd
@@ -114,8 +114,8 @@ func _get_direction() -> Vector2:
 		if current_assignment:
 			if navigation_agent.target_position != current_assignment.global_position:
 				navigation_agent.set_target_position(current_assignment.global_position)
-		if not navigation_agent.is_target_reached():
-			direction = global_position.direction_to(navigation_agent.get_next_path_position())
+			if not navigation_agent.is_target_reached():
+				direction = global_position.direction_to(navigation_agent.get_next_path_position())
 	elif current_assignment:
 		direction = global_position.direction_to(current_assignment.global_position)
 	return direction
@@ -179,3 +179,100 @@ func change_morale(change):
 		_on_demoralized()
 	elif is_distracted and morale >= max_morale:
 		_on_remoralized()
+
+# TODO: Definitely a way to consolidate the "stop_X" functions, but not worth the effort right now
+
+func stop_patching():
+	_set_assignment(null)
+	state = State.IDLE
+	
+	var closest_leak = _get_closest(get_tree().get_nodes_in_group("leak"))
+	
+	while not is_instance_valid(closest_leak) and state == State.IDLE:
+		if morale > 0:
+			await get_tree().create_timer(.1).timeout
+			closest_leak = _get_closest(get_tree().get_nodes_in_group("leak"))
+		else:
+			break
+	
+	if not is_instance_valid(current_assignment):
+		set_assignment(closest_leak)
+	
+	if closest_leak and _check_in_range(closest_leak):
+		_start_assignment()
+
+func stop_bailing():
+	_set_assignment(null)
+	state = State.IDLE
+	
+	var closest_puddle = _get_closest(get_tree().get_nodes_in_group("puddle"))
+	
+	while not is_instance_valid(closest_puddle) and state == State.IDLE:
+		if morale > 0:
+			await get_tree().create_timer(.1).timeout
+			closest_puddle = _get_closest(get_tree().get_nodes_in_group("puddle"))
+		else:
+			break
+	
+	if not is_instance_valid(current_assignment):
+		set_assignment(closest_puddle)
+		
+	if closest_puddle and _check_in_range(closest_puddle):
+		_start_assignment()
+
+func stop_fighting():
+	_set_assignment(null)
+	state = State.IDLE
+	
+	var closest_pirate = _get_closest(get_tree().get_nodes_in_group("pirate"))
+	
+	while not is_instance_valid(closest_pirate) and state == State.IDLE:
+		if morale > 0:
+			await get_tree().create_timer(.1).timeout
+			closest_pirate = _get_closest(get_tree().get_nodes_in_group("pirate"))
+		else:
+			break
+	
+	if not is_instance_valid(current_assignment):
+		set_assignment(closest_pirate)
+	
+	if closest_pirate and _check_in_range(closest_pirate):
+		_start_assignment()
+
+func stop_stomping_rat():
+	_set_assignment(null)
+	state = State.IDLE
+	
+	var closest_rat = _get_closest(get_tree().get_nodes_in_group("rat"))
+	
+	while not is_instance_valid(closest_rat) and state == State.IDLE:
+		if morale > 0:
+			await get_tree().create_timer(.1).timeout
+			closest_rat = _get_closest(get_tree().get_nodes_in_group("rat"))
+		else:
+			break
+	
+	if not is_instance_valid(current_assignment):
+		set_assignment(closest_rat)
+	
+	if closest_rat and _check_in_range(closest_rat):
+		_start_assignment()
+
+func stop_repelling_seagull():
+	_set_assignment(null)
+	state = State.IDLE
+	
+	var closest_seagull = _get_closest(get_tree().get_nodes_in_group("seagull"))
+	
+	while not is_instance_valid(closest_seagull) and state == State.IDLE:
+		if morale > 0:
+			await get_tree().create_timer(.1).timeout
+			closest_seagull = _get_closest(get_tree().get_nodes_in_group("seagull"))
+		else:
+			break
+	
+	if not is_instance_valid(current_assignment):
+		set_assignment(closest_seagull)
+	
+	if closest_seagull and _check_in_range(closest_seagull):
+		_start_assignment()

Custom Resources/SaveData/default_save_data.tres
@@ -11,7 +11,7 @@
 [ext_resource type="Resource" uid="uid://bb034mnapr3eb" path="res://Custom Resources/Stages/stage_6_data.tres" id="9_lvqo3"]
 [ext_resource type="Resource" uid="uid://jo0v2yvpn654" path="res://Custom Resources/Stages/stage_7_data.tres" id="10_841gg"]
 [ext_resource type="Resource" uid="uid://by62v3qd7i5bf" path="res://Custom Resources/Stages/stage_8_data.tres" id="11_c84cx"]
-[ext_resource type="Resource" uid="uid://dq780da12m0vl" path="res://Custom Resources/Stages/stage_9_data.tres" id="12_nei7w"]
+[ext_resource type="Resource" path="res://Custom Resources/Stages/stage_9_data.tres" id="12_nei7w"]
 [ext_resource type="Resource" uid="uid://dyt7qu6b0b6x2" path="res://Custom Resources/Stages/stage_10_data.tres" id="13_ahj1i"]
 
 [resource]

SCRIPTS/worker.gd
@@ -73,7 +73,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _idle_state(delta: float):
-	if current_assignment:
+	if is_instance_valid(current_assignment):
 		if _check_in_range(get_current_target()):
 			if current_assignment is Player:
 				_start_assignment_player()
@@ -182,7 +182,7 @@ func _set_assignment(new_assignment: Node2D):
 	var old_assignment = current_assignment
 	current_assignment = new_assignment
 	
-	if old_assignment:
+	if is_instance_valid(old_assignment):
 		if old_assignment.is_connected("died", _on_assignment_died):
 			old_assignment.disconnect("died", _on_assignment_died)
 		
@@ -288,7 +288,7 @@ func stop_patching():
 			_start_assignment()
 
 func _patch_leak() -> void:
-	if current_assignment and current_assignment is Leak:
+	if is_instance_valid(current_assignment) and current_assignment is Leak:
 		current_assignment.patch()
 
 func start_fighting():

commit 2f9bdefccd7aee8653549b0d53389f6de03a87f0

Instead of looking at state, check current assignment
Bug was introduced by commit 2f51f71d20f56b3cdadb7422a09329ef22371c19

SCRIPTS/crew_member.gd
@@ -117,8 +117,8 @@ func _get_direction() -> Vector2:
 		if current_assignment:
 			if navigation_agent.target_position != current_assignment.global_position:
 				navigation_agent.set_target_position(current_assignment.global_position)
-			if not navigation_agent.is_target_reached():
-				direction = global_position.direction_to(navigation_agent.get_next_path_position())
+		if not navigation_agent.is_target_reached():
+			direction = global_position.direction_to(navigation_agent.get_next_path_position())
 	elif current_assignment:
 		direction = global_position.direction_to(current_assignment.global_position)
 	return direction
@@ -191,7 +191,7 @@ func stop_patching():
 	
 	var closest_leak = _get_closest(get_tree().get_nodes_in_group("leak"))
 	
-	while not is_instance_valid(closest_leak) and state == State.IDLE:
+	while not is_instance_valid(closest_leak) and current_assignment == null:
 		if morale > 0:
 			await get_tree().create_timer(.1).timeout
 			closest_leak = _get_closest(get_tree().get_nodes_in_group("leak"))
@@ -210,7 +210,7 @@ func stop_bailing():
 	
 	var closest_puddle = _get_closest(get_tree().get_nodes_in_group("puddle"))
 	
-	while not is_instance_valid(closest_puddle) and state == State.IDLE:
+	while not is_instance_valid(closest_puddle) and current_assignment == null:
 		if morale > 0:
 			await get_tree().create_timer(.1).timeout
 			closest_puddle = _get_closest(get_tree().get_nodes_in_group("puddle"))
@@ -229,7 +229,7 @@ func stop_fighting():
 	
 	var closest_pirate = _get_closest(get_tree().get_nodes_in_group("pirate"))
 	
-	while not is_instance_valid(closest_pirate) and state == State.IDLE:
+	while not is_instance_valid(closest_pirate) and current_assignment == null:
 		if morale > 0:
 			await get_tree().create_timer(.1).timeout
 			closest_pirate = _get_closest(get_tree().get_nodes_in_group("pirate"))
@@ -248,7 +248,7 @@ func stop_stomping_rat():
 	
 	var closest_rat = _get_closest(get_tree().get_nodes_in_group("rat"))
 	
-	while not is_instance_valid(closest_rat) and state == State.IDLE:
+	while not is_instance_valid(closest_rat) and current_assignment == null:
 		if morale > 0:
 			await get_tree().create_timer(.1).timeout
 			closest_rat = _get_closest(get_tree().get_nodes_in_group("rat"))
@@ -267,7 +267,7 @@ func stop_repelling_seagull():
 	
 	var closest_seagull = _get_closest(get_tree().get_nodes_in_group("seagull"))
 	
-	while not is_instance_valid(closest_seagull) and state == State.IDLE:
+	while not is_instance_valid(closest_seagull) and current_assignment == null:
 		if morale > 0:
 			await get_tree().create_timer(.1).timeout
 			closest_seagull = _get_closest(get_tree().get_nodes_in_group("seagull"))