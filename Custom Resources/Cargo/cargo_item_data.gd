8793bb01892550f5d0fbb4c7f73dfeba952dd1cd
avelitj12 <avelitj12@gmail.com>
2026-02-15 11:26:17 -0500

    Add ability to load action icon for cargo
    Add them for livestock and siren

--- Custom Resources/Cargo/cargo_item_data.gd
+++ Custom Resources/Cargo/cargo_item_data.gd
@@ -6,5 +6,6 @@ class_name CargoItemData
 @export var health := 0
 @export var number_items := 0
 @export var texture: Texture2D
+@export var action_button_icon: Texture2D
 @export var container_texture: Texture2D
 @export var is_distraction: bool
